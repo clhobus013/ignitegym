f8db2a29edd4c6033c9ff2259ab661eccd4c6d37
claudia <clauu013@gmail.com>
2025-02-06 17:06:30 -0300

    Add imagem de background

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import { StatusBar, View } from 'react-native';
-import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { useFonts } from 'expo-font';
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
-import { config } from './config/gluestack-ui.config';
+import { StatusBar } from "react-native";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { useFonts } from "expo-font";
+import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { config } from "./config/gluestack-ui.config";
+import { Loading } from "@components/Loading";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
 
@@ -14,16 +16,13 @@ export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
+        barStyle="light-content"
+        backgroundColor="transparent"
         translucent
       />
       {fontsLoaded ? (
-        <Center flex={1} bg='$gray700'>
-          <Text>Home</Text>
-
-        </Center>
-      ) : <View></View>}
+        <SignIn/>
+      ) : <Loading/>}
     </GluestackUIProvider>
   );
 }
