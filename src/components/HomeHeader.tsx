11e1a34ffa726e9e9623bdb2ed75327d055b0edc
claudia <clauu013@gmail.com>
2025-02-09 16:18:37 -0300

    Add homeHeader componente

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,19 @@
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+    return (
+        <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+            <UserPhoto
+                source={{uri: "https://github.com/clhobus013.png"}}
+                w="$16"
+                h="$16"
+                alt="Foto de perfil"
+            />
+            <VStack>
+                <Text color="$gray100" fontSize="$sm">Olá, </Text>
+                <Heading color="$gray100" fontSize="$md">João</Heading>
+            </VStack>
+        </HStack>
+    )
+}
