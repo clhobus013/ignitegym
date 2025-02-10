55e1487f9b329b3169ee927dc5ac659c35ced970
claudia <clauu013@gmail.com>
2025-02-10 16:55:28 -0300

    Add listagem dos exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,8 +9,10 @@ export function Home() {
     const [groupSelected, setGroupSelected] = useState<string>("costas");
     const [groups, setGroups] = useState<string[]>(["costas", "ombro", "perna", "abdomem", "peito"]);
 
+    const [exercises, setExercises] = useState<string[]>(["Puxada Frontal", "Desenvolvimento", "Agachamento", "Prancha", "1", "2", "3"]);
+
     return (
-        <VStack>
+        <VStack flex={1}>
             <HomeHeader/>
 
             <FlatList
@@ -29,14 +31,21 @@ export function Home() {
                 style={{ marginVertical: 40, maxHeight: 44, minHeight: 44}}
             />
 
-            <VStack px="$8">
+            <VStack px="$8" flex={1}>
                 <HStack justifyContent="space-between" mb="$5" alignItems="center">
                     <Heading color="$gray200" fontSize="$md" fontFamily="$heading">Exercícios</Heading>
-                    <Text color="$gray200" fontSize="$sm" fontFamily="$body">4</Text>
+                    <Text color="$gray200" fontSize="$sm" fontFamily="$body">{exercises.length}</Text>
                 </HStack>
 
-                <ExerciseCard/>
-
+                <FlatList
+                    data={exercises}
+                    keyExtractor={(item) => item}
+                    renderItem={({item}) => (
+                        <ExerciseCard/>
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 20}}    
+                />
             </VStack>
         </VStack>
     )
