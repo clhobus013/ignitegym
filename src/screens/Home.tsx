6a1e69fb5165aff9d6e4ca605d92ce464b9b3258
claudia <clauu013@gmail.com>
2025-02-10 15:51:09 -0300

    Add listagem dos grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,32 @@
+import { Group } from "@components/Group"
 import { HomeHeader } from "@components/HomeHeader"
-import { Center, VStack } from "@gluestack-ui/themed"
+import { VStack } from "@gluestack-ui/themed"
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+    const [groupSelected, setGroupSelected] = useState<string>("costas");
+    const [groups, setGroups] = useState<string[]>(["costas", "ombro", "perna", "abdomem", "peito"]);
+
     return (
         <VStack>
             <HomeHeader/>
+
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <Group 
+                        name={item} 
+                        isActive={groupSelected === item} 
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{paddingHorizontal: 32}}
+                style={{ marginVertical: 40, maxHeight: 44, minHeight: 44}}
+            />
         </VStack>
     )
 }
