bf31a972a76f0bf5f9881d2d11c6d29dc2081c36
claudia <clauu013@gmail.com>
2025-02-12 12:21:42 -0300

    Add inputs do perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,11 @@
-import { ScreenHeader } from "@components/ScreenHeader"
-import { UserPhoto } from "@components/UserPhoto"
-import { Center, Text, VStack } from "@gluestack-ui/themed"
 import { ScrollView, TouchableOpacity } from "react-native"
+
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed"
+
+import { Input } from "@components/Input"
+import { Button } from "@components/Button"
+import { UserPhoto } from "@components/UserPhoto"
+import { ScreenHeader } from "@components/ScreenHeader"
 
 export function Profile() {
     return (
@@ -10,8 +14,10 @@ export function Profile() {
 
             <ScrollView contentContainerStyle={{paddingBottom: 36}}>
                 <Center mt="$6" px="$10">
-                    <UserPhoto source={{uri: "https:github.com/clhobus013.png"}} alt="Foto de perfil do usuário"
-                    size="xl"/>
+                    <UserPhoto
+                        source={{uri: "https:github.com/clhobus013.png"}} alt="Foto de perfil do usuário"
+                        size="xl"
+                    />
 
                     <TouchableOpacity>
                         <Text 
@@ -24,6 +30,47 @@ export function Profile() {
                             Alterar Foto
                         </Text>
                     </TouchableOpacity>
+
+                    <Center w="$full" gap="$4">
+                        <Input placeholder="Nome" bg="$gray600"/>
+                        <Input 
+                            value="clauu013@gmail.com" 
+                            bg="$gray600" 
+                            isReadOnly
+                        />
+                    </Center>
+
+                    <Heading 
+                        alignSelf="flex-start" 
+                        fontFamily="$heading" 
+                        color="$gray200" 
+                        fontSize="$md" 
+                        mt="$12" 
+                        mb="$2"
+                    >
+                        Alterar senha
+                    </Heading>
+
+                    <Center w="$full" gap="$4">
+                        <Input 
+                            placeholder="Senha antiga" 
+                            bg="$gray600" 
+                            secureTextEntry
+                        />
+                        <Input 
+                            placeholder="Nova senha" 
+                            bg="$gray600" 
+                            secureTextEntry
+                        />
+                        <Input 
+                            placeholder="Confirme a nova senha" 
+                            bg="$gray600" 
+                            secureTextEntry
+                        />
+
+                        <Button title="Atualizar"/>
+                    </Center>
+
                 </Center>
             </ScrollView>
         </VStack>
