7f16089d55acd7e34561687432ab6ca5ea2a81ab
claudia <clauu013@gmail.com>
2025-02-12 12:08:55 -0300

    Add profile screen layout with header and user photo component

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,31 @@
-import { Center, Text } from "@gluestack-ui/themed"
+import { ScreenHeader } from "@components/ScreenHeader"
+import { UserPhoto } from "@components/UserPhoto"
+import { Center, Text, VStack } from "@gluestack-ui/themed"
+import { ScrollView, TouchableOpacity } from "react-native"
 
 export function Profile() {
     return (
-        <Center flex={1}>
-            <Text color="$white">Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Perfil"/>
+
+            <ScrollView contentContainerStyle={{paddingBottom: 36}}>
+                <Center mt="$6" px="$10">
+                    <UserPhoto source={{uri: "https:github.com/clhobus013.png"}} alt="Foto de perfil do usuário"
+                    size="xl"/>
+
+                    <TouchableOpacity>
+                        <Text 
+                            color="$green500" 
+                            fontFamily="$heading" 
+                            fontSize="$md" 
+                            mt="$2" 
+                            mb="$8"
+                        >
+                            Alterar Foto
+                        </Text>
+                    </TouchableOpacity>
+                </Center>
+            </ScrollView>
+        </VStack>
     )
 }
