ea43c7766b528b6973d94cdf94832010ccfbf46a
claudia <clauu013@gmail.com>
2025-02-12 11:45:15 -0300

    Add lista de historico de exercicios

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,46 @@
 import { HistoryCard } from "@components/HistoryCard"
 import { ScreenHeader } from "@components/ScreenHeader"
-import { VStack } from "@gluestack-ui/themed"
+import { Heading, Text, VStack } from "@gluestack-ui/themed"
+import { useState } from "react";
+import { SectionList } from "react-native";
 
 export function History() {
+    const [exercises, setExercises] = useState([
+        {title: "12.02.25", data: ["Puxada Frontal"]},
+        {title: "10.02.25", data: ["Puxada Frontal", "Remada unilateral"]},
+    ]);
     return (
         <VStack flex={1}>
             <ScreenHeader title="Histórico de Exercícios"/>
-            <HistoryCard/>
+
+            <SectionList
+                sections={exercises}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <HistoryCard/>
+                )}
+                renderSectionHeader={({section}) => (
+                    <Heading 
+                        color="$gray200" 
+                        fontSize="$md" 
+                        mt="$10" 
+                        mb="$3" 
+                        fontFamily="$heading"
+                    >
+                        {section.title}
+                    </Heading>
+                )}
+                style={{paddingHorizontal: 32}}
+                contentContainerStyle={
+                    exercises.length === 0 && {flex: 1, justifyContent: "center", }
+                }
+                ListEmptyComponent={()=> (
+                    <Text color="$gray100" textAlign="center">
+                        Não há exercícios registrados ainda. {"\n"} Vamos fazer exercícios hoje ?
+                    </Text>
+                )}
+                showsVerticalScrollIndicator={false}
+            />
         </VStack>
     )
 }
