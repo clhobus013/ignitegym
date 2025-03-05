97c5e08eb561e7affb905ee01d6cc2f73d8a40eb
Claudia Hobus <clauu013@gmail.com>
2025-03-05 17:44:36 -0300

    Add loading na tela de historico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -10,6 +10,7 @@ import { api } from "../service/api";
 import { HistoryCard } from "@components/HistoryCard"
 import { ScreenHeader } from "@components/ScreenHeader"
 import { ToastMessage } from "@components/ToastMessage";
+import { Loading } from "@components/Loading";
 
 
 export function History() {
@@ -48,34 +49,36 @@ export function History() {
         <VStack flex={1}>
             <ScreenHeader title="Histórico de Exercícios"/>
 
-            <SectionList
-                sections={exercises}
-                keyExtractor={(item) => item.id}
-                renderItem={({item}) => (
-                    <HistoryCard data={item}/>
-                )}
-                renderSectionHeader={({section}) => (
-                    <Heading 
-                        color="$gray200" 
-                        fontSize="$md" 
-                        mt="$10" 
-                        mb="$3" 
-                        fontFamily="$heading"
-                    >
-                        {section.title}
-                    </Heading>
-                )}
-                style={{paddingHorizontal: 32}}
-                contentContainerStyle={
-                    exercises.length === 0 && {flex: 1, justifyContent: "center", }
-                }
-                ListEmptyComponent={()=> (
-                    <Text color="$gray100" textAlign="center">
-                        Não há exercícios registrados ainda. {"\n"} Vamos fazer exercícios hoje ?
-                    </Text>
-                )}
-                showsVerticalScrollIndicator={false}
-            />
+            { isLoading ? <Loading/> : (
+                <SectionList
+                    sections={exercises}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({item}) => (
+                        <HistoryCard data={item}/>
+                    )}
+                    renderSectionHeader={({section}) => (
+                        <Heading 
+                            color="$gray200" 
+                            fontSize="$md" 
+                            mt="$10" 
+                            mb="$3" 
+                            fontFamily="$heading"
+                        >
+                            {section.title}
+                        </Heading>
+                    )}
+                    style={{paddingHorizontal: 32}}
+                    contentContainerStyle={
+                        exercises.length === 0 && {flex: 1, justifyContent: "center", }
+                    }
+                    ListEmptyComponent={()=> (
+                        <Text color="$gray100" textAlign="center">
+                            Não há exercícios registrados ainda. {"\n"} Vamos fazer exercícios hoje ?
+                        </Text>
+                    )}
+                    showsVerticalScrollIndicator={false}
+                />
+            )}
         </VStack>
     )
 }
